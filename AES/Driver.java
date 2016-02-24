87301558743c0f6cb7e8de34db71fa34bcd63914
Siva Rama Krishna Prasad Chintapalli <chintapallisiva90@gmail.com>
2016-02-23 22:18:26 -0500

    Siva Chintapalli,02-23-2016,Lab2
    Final Code Changes with Indentations

---
+++ AES/Driver.java
@@ -0,0 +1,36 @@
+/**
+ * file: Driver.java
+ * author: SivaChintapalli
+ * course: MSCS_630L_231_16S
+ * assignment: Lab 2
+ * due date: February 23, 2016
+ * version: 1.0
+ */
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Driver {
+ static HashMap<Integer,String> roundKey = new HashMap<>();
+    public static void main(String[] args) {
+        // takes the input from the user and stores in to the key
+    Scanner input = new Scanner(System.in);
+    String key = input.next();
+    //checking the user input is in a 32digits hexadecimal format or not  
+    if(key.matches("[0-9A-F]{32}")){
+       AESChiper aesChiper = new AESChiper();
+// storing 11 round keys in to roundKey HashMap    
+    roundKey =aesChiper.aesRoundKey(key); 
+    //printing the 11 round keys 
+        for(int i=0;i<=roundKey.size();i++){
+            System.out.println(roundKey.get(i));
+        }
+    } 
+    //if the input from user is not in hexadecimal format terminating the program
+    else {
+        System.out.println("Invalid input key ");
+     }
+    
+    
+    }
+}
