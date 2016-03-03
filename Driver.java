ffe10c0f2778344de05f80a082aa1fb1782d0035
SivaChintapalli1 <Siva.Chintapalli1@marist.edu>
2016-03-03 10:55:32 -0500

    Added files via upload

--- Driver.java
+++ Driver.java
@@ -1,36 +1,45 @@
-/**
- * file: Driver.java
- * author: SivaChintapalli
- * course: MSCS_630L_231_16S
- * assignment: Lab 2
- * due date: February 23, 2016
- * version: 1.0
- */
-
-import java.util.HashMap;
 import java.util.Scanner;
-
-public class Driver {
- static HashMap<Integer,String> roundKey = new HashMap<>();
-    public static void main(String[] args) {
-        // takes the input from the user and stores in to the key
-    Scanner input = new Scanner(System.in);
-    String key = input.next();
-    //checking the user input is in a 32digits hexadecimal format or not  
-    if(key.matches("[0-9A-F]{32}")){
-       AESChiper aesChiper = new AESChiper();
-// storing 11 round keys in to roundKey HashMap    
-    roundKey =aesChiper.aesRoundKey(key); 
-    //printing the 11 round keys 
-        for(int i=0;i<=roundKey.size();i++){
-            System.out.println(roundKey.get(i));
-        }
-    } 
-    //if the input from user is not in hexadecimal format terminating the program
-    else {
-        System.out.println("Invalid input key ");
-     }
-    
-    
+public class Driver{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+String m =input.nextLine();
+while(input.hasNext()){
+m = m+"\n"+ input.nextLine();
+}
+System.out.println(m);
+String mHex =mToHex(m);
+System.out.println(mHex);
+String mHexBlocks[][] =mHexToBlocks(mHex, 128);
+for (int i = 0; i < 4; i++) {
+System.out.println();
+      for (int j = 0; j < 4; j++) {
+System.out.print(mHexBlocks[i][j]);
+}
+}
+}
+protected static String mToHex(String m){
+StringBuilder temp = new StringBuilder();
+for (char ch: m.toCharArray()) {
+    if (temp.length() > 0)
+      temp.append(' ');
+    temp.append(String.format("%2x", (int) ch));
+  }
+  return temp.toString();
+}
+protected static String[][] mHexToBlocks(String mHex,int bSize){
+int matrixSize = bSize/mHex.length();
+int row = matrixSize;
+    int coloumn = matrixSize;
+    String[][] mHexBlock = new String[row][coloumn];
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < coloumn; j++) {
+        // coping  input key into 4*4 matrix
+        mHexBlock[i][j] = String.valueOf(mHex.charAt(row*i
++j));
+        
+      }
     }
+
+    return mHexBlock;
+}
 }
